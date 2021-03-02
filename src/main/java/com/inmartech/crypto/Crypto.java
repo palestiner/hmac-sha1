94d44bddd9e3e990acd37ca7ecdcc0a2d9c2b6a1
Медведев Юрий Владимирович <muv@cs-consult.ru>
2021-03-02 17:43:54 +0700

    null safety

--- src/main/java/com/inmartech/crypto/Crypto.java
+++ src/main/java/com/inmartech/crypto/Crypto.java
@@ -23,6 +23,7 @@ public final class Crypto {
 
     public static String generateSignature(String body, String secret)
             throws NoSuchAlgorithmException, InvalidKeyException {
+        if (body == null || secret == null) return null;
         SecretKeySpec signingKey = new SecretKeySpec(secret.getBytes(), HMAC_SHA1_ALGORITHM);
         Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
         mac.init(signingKey);
